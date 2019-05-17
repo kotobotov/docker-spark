2f1527386d99c9654c55e524df4d83685b883357
dmitriy.kubitskiy <dkubitskiy@innodata.ru>
2019-05-17 17:58:35 +0300

    :construction_worker: Dynseg version

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-TAG=2.4.0-hadoop2.7
+TAG=2.2.0-hadoop2.7
 
 build() {
     NAME=$1
-    IMAGE=bde2020/spark-$NAME:$TAG
+    IMAGE=kotobotov/spark-$NAME:$TAG
     cd $([ -z "$2" ] && echo "./$NAME" || echo "$2")
     echo '--------------------------' building $IMAGE in $(pwd)
     docker build -t $IMAGE .
